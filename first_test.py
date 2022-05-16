48edbebf9928b211aab05b61f2ac01df5640bdfa
Pegassuus <auguste.pousette@gmail.com>
2022-05-16 14:51:23 +0200

    Min första commit!!!

---
+++ first_test.py
@@ -0,0 +1,13 @@
+#This is the first for loop program
+
+list_of_fruits = ["apple", "banana", "cherry"]
+
+hejhej = 1
+res = hejhej + 123
+print(res)
+
+for fruit in list_of_fruits:
+    print(fruit)
+
+    for mycharacter in fruit:
+        print(mycharacter)
